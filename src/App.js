47e34fddf21970d6fb4bb9571e89e3111fd2ff81
delfina <clair.daisies@gmail.com>
2022-06-04 18:29:02 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './Index.css';
+import './App.css';
+function App() {
+  return (
+    <div className='App'>
+      <div className='blur' style={{ top: '-10%', right: '0' }}></div>
+      <div className='blur' style={{ top: '33%', left: '-8rem'}}></div>
+    </div>
+  );
+}
+
+export default App;
