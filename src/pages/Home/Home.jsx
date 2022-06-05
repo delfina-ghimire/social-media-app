b2a8bae14bb25ca8c6712892a1468bdfc52e5558
delfina <clair.daisies@gmail.com>
2022-06-05 10:23:12 +0545

    Profile section skelaton

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProfileSide from '../../components/ProfileSide/ProfileSide';
 import './Home.css';
 
 const Home = () => {
   return (
-    <div className='home'>
-      <div className='profileSide'>profile</div>
+    <div className='Home'>
+      <ProfileSide />
       <div className='postSide'>post</div>
       <div className='infoSide'>info</div>
     </div>
