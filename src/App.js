40ec414d889a677b399bbbd7b8967b61f40bd7e4
delfina <clair.daisies@gmail.com>
2022-06-05 06:07:28 +0545

    Homepage skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './Index.css';
 import './App.css';
+import Home from './pages/Home/Home';
 function App() {
   return (
     <div className='App'>
       <div className='blur' style={{ top: '-10%', right: '0' }}></div>
-      <div className='blur' style={{ top: '33%', left: '-8rem'}}></div>
+      <div className='blur' style={{ top: '33%', left: '-8rem' }}></div>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='profileSide'>profile</div>
+      <div className='postSide'>post</div>
+      <div className='infoSide'>info</div>
+    </div>
+  );
+};
+
+export default Home;
