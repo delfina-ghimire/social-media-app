dcda006d2e2df12f4aa72dc7564354741f8c7052
delfina <clair.daisies@gmail.com>
2022-06-07 05:48:04 +0545

    profile card

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
+import cover from '../../img/cover.jpg';
+import profileImg from '../../img/profileImg.jpg';
+
+import './ProfileCard.css';
+
 const ProfileCard = () => {
-  return <div>ProfileCard</div>;
+  return (
+    <div className='profileCard'>
+      <div className='profileImgs'>
+        <img src={cover} v alt='cover photo' className='coverImg' />
+        <img src={profileImg} alt='profile photo' className='profileImg' />
+      </div>
+
+      <div className='profileName'>
+        <span> Alexandria Hem</span>
+        <span>Fullstack Developer</span>
+      </div>
+
+      <div className='followStatus'>
+        <hr />
+        <div>
+          <div className='follow'>
+            <span>6782</span>
+            <span>Followers</span>
+          </div>
+          <div className='verticleLine'></div>
+          <div className='follow'>
+            <span>678</span>
+            <span>Followings</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+
+      <span>My Profile</span>
+    </div>
+  );
 };
 
 export default ProfileCard;
