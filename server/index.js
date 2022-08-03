a0b8c7174d892be03933865cfe1cc8cc9cc192ad
delfina <clair.daisies@gmail.com>
2022-08-03 13:10:27 +0545

    Setting up server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+
+const app = express();
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
