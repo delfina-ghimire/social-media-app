d978c1fe90ff03dfd741bd50a0414a438e2d7292
delfina <clair.daisies@gmail.com>
2022-08-04 11:51:58 +0545

    Setting up the server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
-app.use(bodyParser.json({limit: '30mb', extended: true}))
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
+dotenv.config();
+const { MONGO_DB, PORT } = process.env;
+mongoose
+  .connect(MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, console.log(`Listening at ${PORT}`))).catch((error) => console.log(error));
