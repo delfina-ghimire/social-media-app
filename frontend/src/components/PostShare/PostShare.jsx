dca9be8fdce1c2917fc5fcbee347f85265bceb98
delfina <clair.daisies@gmail.com>
2022-08-11 19:07:58 +0545

    Share Post

---
+++ frontend/src/components/PostShare/PostShare.jsx
@@ -0,0 +1,138 @@
+import {
+  UilLocationPoint,
+  UilPlayCircle,
+  UilScenery,
+  UilSchedule,
+  UilTimes,
+} from '@iconscout/react-unicons';
+import { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { uploadImage, uploadPost } from '../../actions/uploadAction';
+import ProfileImg from '../../img/profileImg.jpg';
+import './PostShare.css';
+
+const PostShare = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const loading = useSelector((state) => state.postReducer.uploading);
+  const imageRef = useRef();
+  const [image, setImage] = useState(null);
+  const desc = useRef();
+
+  //handle Image Change
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      setImage(img);
+    }
+  };
+
+  const reset = () => {
+    setImage(null);
+    desc.current.value = '';
+  };
+
+  //handle post upload
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+
+    //handle image upload
+    if (image) {
+      const data = new FormData();
+      const filename = Date.now() + image.name;
+      data.append('name', filename);
+      data.append('file', image);
+      newPost.image = filename;
+      console.log(newPost);
+
+      try {
+        dispatch(uploadImage(data));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    dispatch(uploadPost(newPost));
+    reset();
+  };
+
+  return (
+    <div className='PostNav'>
+      <img src={ProfileImg} alt='profile' />
+      <div className='postNav__container'>
+        <input
+          ref={desc}
+          required
+          className='postInput__input'
+          type='text'
+          placeholder="What's in your mind?"
+        />
+        <div className='postOptions'>
+          <div
+            className='postOptions__option'
+            style={{ color: 'var(--photo)' }}
+            onClick={() => imageRef.current.click()}
+          >
+            <UilScenery />
+            Photo
+          </div>
+          <div
+            className='postOptions__option'
+            style={{ color: 'var(--video)' }}
+          >
+            <UilPlayCircle />
+            Video
+          </div>
+          <div
+            className='postOptions__option'
+            style={{ color: 'var(--location)' }}
+          >
+            <UilLocationPoint />
+            Location
+          </div>
+          <div
+            className='postOptions__option'
+            style={{ color: 'var(--shedule)' }}
+          >
+            <UilSchedule />
+            Schedule
+          </div>
+          <button
+            className='btn btn--postShare'
+            onClick={handleSubmit}
+            disabled={loading}
+          >
+            {loading ? 'Uploading...' : 'Share'}
+          </button>
+          <div style={{ display: 'none' }}>
+            <input
+              type='file'
+              name='myImage'
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
+        </div>
+
+        {image && (
+          <div className='previewImg__container'>
+            <UilTimes
+              className='previewImg__icon'
+              onClick={() => setImage(null)}
+            />
+            <img
+              className='previewImg'
+              src={URL.createObjectURL(image)}
+              alt=''
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PostShare;
