8ae98ff6333971f34449d9b71fa167ab36e3c41c
delfina <clair.daisies@gmail.com>
2022-08-17 17:46:55 +0545

    Like and unlike posts

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { likePost } from '../../api/PostRequest';
 import Comment from '../../img/comment.png';
 import Heart from '../../img/like.png';
 import NotLiked from '../../img/notlike.png';
@@ -7,6 +9,15 @@ import './Post.css';
 
 const Post = ({ data }) => {
   const { user } = useSelector((state) => state.authReducer.authData);
+  const [liked, setLiked] = useState(data.likes.includes(user._id));
+  const [likes, setLikes] = useState(data.likes.length);
+
+  const handleLike = () => {
+    setLiked((prev) => !prev);
+    likePost(data._id, user._id);
+    liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+  };
+
   return (
     <div className='post'>
       <img
@@ -16,13 +27,18 @@ const Post = ({ data }) => {
       />
 
       <div className='post__reaction'>
-        <img src={data.liked ? Heart : NotLiked} alt='liked' />
+        <img
+          src={liked ? Heart : NotLiked}
+          alt='liked'
+          style={{ cursor: 'pointer' }}
+          onClick={handleLike}
+        />
         <img src={Comment} alt='' />
         <img src={Share} alt='' />
       </div>
 
       <span style={{ color: 'var(--gray)', fontSize: '14px' }}>
-        {data.likes} Likes
+        {likes} Likes
       </span>
       <div className='post__details'>
         <span>
