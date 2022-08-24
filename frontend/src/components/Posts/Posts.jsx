3a7970c3c4877bc8d53c3ca3251c7663c576190a
delfina <clair.daisies@gmail.com>
2022-08-24 18:19:26 +0545

    Testing 123

--- frontend/src/components/Posts/Posts.jsx
+++ frontend/src/components/Posts/Posts.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { getTimelinePosts } from '../../actions/PostAction';
 import Post from '../Post/Post';
 import './Posts.css';
@@ -8,14 +9,14 @@ const Posts = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.authReducer.authData);
   let { posts, loading } = useSelector((state) => state.postReducer);
+  const params = useParams();
 
   useEffect(() => {
     dispatch(getTimelinePosts(user._id));
   }, []);
 
-  if (!posts) {
-    ('No posts to show');
-  }
+  if (!posts) return 'Follow some friends to see their posts!';
+  if (params.id) posts = posts.filter((post) => post.userId === params.id);
   return (
     <div className='posts'>
       {loading
