213587e64550ac07cd0053c535a9fd8e122240f6
rupesh <rupesh.shrestha96742@gmail.com>
2022-09-14 11:43:46 +0545

    Change backend server

--- frontend/src/api/UserRequest.js
+++ frontend/src/api/UserRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://ec2-43-205-99-230.ap-south-1.compute.amazonaws.com:5000",
+  baseURL: "http://ec2-13-232-223-75.ap-south-1.compute.amazonaws.com:5000",
 });
 
 API.interceptors.request.use((req) => {
